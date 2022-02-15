81c322b97b423c98e8ada64110beba2776dde36f
Massimo Endrighi <massimo.endrighi@geopartner.it>
2022-02-15 17:14:43 +0100

    initial commit

---
+++ src/CallBackInfo.py
@@ -0,0 +1,11 @@
+﻿# -*- coding: utf-8 -*-
+
+class CallBackInfo:
+    def __init__(self, objectType, oid, status, progress = None):
+        self.objectType = objectType
+        self.oid = oid 
+        self.status = status
+        self.progress = progress
+        self.errors = None
+        self.error_table = None
+        self.fatal = False
---
+++ src/WaitingBarTreeWidgetItem.py
@@ -0,0 +1,16 @@
+﻿from qgis.PyQt import QtWidgets
+
+class WaitingBarTreeWidgetItem (QtWidgets.QTreeWidgetItem):
+    def __init__( self, parent ):
+      
+        ## Init super class ( QtGui.QTreeWidgetItem )
+        super( WaitingBarTreeWidgetItem, self ).__init__(parent)
+
+        self.progressBar = QtWidgets.QProgressBar()
+        
+        self.progressBar.setMinimum(0)
+        self.progressBar.setMaximum(0)
+        self.progressBar.setValue(0)
+        self.progressBar.setStyleSheet('height:15px; margin-right:5px;')
+
+        self.treeWidget().setItemWidget( self, 0, self.progressBar )
